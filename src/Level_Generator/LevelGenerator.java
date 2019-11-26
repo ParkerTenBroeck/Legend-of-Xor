4d5bfbdf8ee4e110a8e2fa530f87f7eb57bf1058
ParkerTenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2019-11-26 13:46:34 -0500

    added TREEEEE

--- src/Level_Generator/LevelGenerator.java
+++ src/Level_Generator/LevelGenerator.java
@@ -6,10 +6,7 @@
 package Level_Generator;
 
 import legend_of_xor.Game.Tile;
-import legend_of_xor.Game.Tiles.dirt;
-import legend_of_xor.Game.Tiles.explosion;
-import legend_of_xor.Game.Tiles.grass;
-import legend_of_xor.Game.Tiles.rainbow;
+import legend_of_xor.Game.Tiles.*;
 
 /**
  *
@@ -27,21 +24,54 @@ public class LevelGenerator {
 
         for (int x = 0; x < xSize; x++) {
 
-            noise = (gen.noise(scale) + 1) * 5;
+            noise = (gen.noise(scale) + 1) * 5 + ((ySize / 2) - 5);
             scale += 3;
 
             for (int y = ySize - 1; y >= ySize - (int) noise; y--) {
 
                 if (y == (ySize - (int) noise)) {
                     temp[y][x] = new grass();
+                    if (Math.random() < 0.07) {
+                        tree(temp, x, y - 1);
+                    }
                 } else {
                     temp[y][x] = new dirt();
                 }
-                if ((y) == ((ySize - (int) noise))) {
-                    temp[y - 1][x] = new explosion();
+                if ((y - 3) > ((ySize - (int) noise))) {
+                    temp[y][x] = new stone();
+                    if (Math.random() < 0.04) {
+                        temp[y][x] = new rainbow();
+                    }
                 }
             }
         }
         return temp;
+    }
+
+    public static void tree(Tile[][] tiles, int xPos, int yPos) {//x and y are the bottom trunk
+        
+        try{
+        tiles[yPos][xPos] = new wood();
+        tiles[yPos - 1][xPos] = new wood();
+        tiles[yPos - 2][xPos] = new wood();
+        tiles[yPos - 3][xPos] = new wood();
+
+        tiles[yPos - 3][xPos] = new leaf();
+        tiles[yPos - 3][xPos + 1] = new leaf();
+        tiles[yPos - 3][xPos - 1] = new leaf();
+        tiles[yPos - 3][xPos + 2] = new leaf();
+        tiles[yPos - 3][xPos - 2] = new leaf();
+        tiles[yPos - 2][xPos + 1] = new leaf();
+        tiles[yPos - 2][xPos - 1] = new leaf();
+        tiles[yPos - 2][xPos + 2] = new leaf();
+        tiles[yPos - 2][xPos - 2] = new leaf();
+        
+        tiles[yPos - 4][xPos + 1] = new leaf();
+        tiles[yPos - 4][xPos] = new leaf();
+        tiles[yPos - 4][xPos - 1] = new leaf();
+        }catch(Exception e){
+            
+        }
+
     }
 }
---
+++ src/legend_of_xor/Game/Tiles/cobblestone.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package legend_of_xor.Game.Tiles;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import legend_of_xor.Game.Tile;
+import legend_of_xor.Renderer.Textures;
+
+/**
+ *
+ * @author parke
+ */
+public class cobblestone implements Tile {
+
+    public static final String NAMEID = cobblestone.class.getName().split("\\.")[3];//name of tile must be unique
+
+    private final int TILESX = 1; //how many tiles there are in the sprite sheet
+    private final int TILESY = 1;
+
+    private final double TILE_X_SCALE = 1;  //x and y scale of each tile
+    private final double TILE_Y_SCALE = 1;
+    
+        private final byte ORIGIN = 0;//0 upper left,1 upper right,2 bottom left,3 bottom right
+
+    BufferedImage image;
+
+    public cobblestone() {
+        image = Textures.getBlockTexture(this);
+    }
+
+    @Override
+    public String getNameID() {
+        return NAMEID;
+    }
+
+    @Override
+    public BufferedImage getTileImage() {
+        return image;
+    }
+
+    @Override
+    public byte getOrigin() {
+        return ORIGIN;
+    }
+
+    @Override
+    public void update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getXScale() {
+        return TILESX * TILE_X_SCALE;
+    }
+
+    @Override
+    public double getYScale() {
+        return TILESY * TILE_Y_SCALE;
+    }
+
+}
