1a4a69a72a4ed25220a1873b10c77c28e689bb0c
ParkerMacDonald <angryshield@gmail.com>
2019-11-28 15:26:16 -0500

    controls changes again

--- src/legend_of_xor/Controls.java
+++ src/legend_of_xor/Controls.java
@@ -1,13 +1,81 @@
-
 package legend_of_xor;
 
-/**
- *
- * @author parke
- */
+import java.awt.event.*;
+
 public class Controls {
 
-    private static int controlType = 0;
+    private static enum controlType {KEYBOARD, CONTROLLER}
     
+    //Keyboard Keys variables
+    private static char jumpKey = 'w', rightKey = 'd', leftKey = 'a', attackKey = 'j', blockKey = 'k', magicKey = ' ';
+            
     
+    //Controller button variables
+    
+    private static controlType controlTypeInUse = controlType.CONTROLLER;
+    
+    public void setControlType(controlType type){
+        controlTypeInUse = type;
+    }
+    
+    public static boolean isLeftPressed(){
+        switch(controlTypeInUse){
+            case KEYBOARD:
+                
+                break;
+            case CONTROLLER:
+                break;
+            default: return false;
+        }
+        return false;
+    }
+    
+    //---------METHODS FOR USER CHANGING AND GETTING THE CONTROLS------------
+    public static void setJumpKey(char key){
+        jumpKey = key;
+    }
+    
+    public static void setRightKey(char key){
+        rightKey = key;
+    }
+    
+    public static void setLeftKey(char key){
+        leftKey = key;
+    }
+    
+    public static void setAttackKey(char key){
+        attackKey = key;
+    }
+    
+    public static void setBlockKey(char key){
+        blockKey = key;
+    }
+    
+    public static void setMagicKey(char key){
+        magicKey = key;
+    }
+    
+    public static char getJumpKey(){
+        return jumpKey;
+    }
+    
+    public static char getRightKey(){
+        return rightKey;
+    }
+    
+    public static char getLeftKey(){
+        return leftKey;
+    }
+    
+    public static char getAttackKey(){
+        return attackKey;
+    }
+    
+    public static char getBlockKey(){
+        return blockKey;
+    }
+    
+    public static char getMagicKey(){
+        return jumpKey;
+    }
 }
--- src/legend_of_xor/KeyboardLisenter.java
+++ src/legend_of_xor/KeyboardLisenter.java
@@ -1,16 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package legend_of_xor;
 
 import legend_of_xor.Veiwer.Frame;
+import java.awt.event.*;
 
-/**
- *
- * @author Parker
- */
-public class KeyboardLisenter {
+public class KeyboardLisenter implements KeyListener{
+    
+    private static boolean jumpPressed = false, rightPressed = false, leftPressed = false, attackPressed = false, blockPressed = false, magicPressed = false;
+    
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        if(ke.getKeyChar() == Controls.getJumpKey()) jumpPressed = true;
+        if(ke.getKeyChar() == Controls.getRightKey()) rightPressed = true;
+        if(ke.getKeyChar() == Controls.getLeftKey()) leftPressed = true;
+        if(ke.getKeyChar() == Controls.getAttackKey()) attackPressed = true;
+        if(ke.getKeyChar() == Controls.getBlockKey()) blockPressed = true;
+        if(ke.getKeyChar() == Controls.getMagicKey()) magicPressed = true;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        if(ke.getKeyChar() == Controls.getJumpKey()) jumpPressed = false;
+        if(ke.getKeyChar() == Controls.getRightKey()) rightPressed = false;
+        if(ke.getKeyChar() == Controls.getLeftKey()) leftPressed = false;
+        if(ke.getKeyChar() == Controls.getAttackKey()) attackPressed = false;
+        if(ke.getKeyChar() == Controls.getBlockKey()) blockPressed = false;
+        if(ke.getKeyChar() == Controls.getMagicKey()) magicPressed = false;
+    }
     
 }
